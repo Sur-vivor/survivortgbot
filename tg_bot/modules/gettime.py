6bfae6275ae570ea222c223ef53d35196dffd1cc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-03-30 12:42:55 +0530

    Update gettime.py

--- tg_bot/modules/gettime.py
+++ tg_bot/modules/gettime.py
@@ -39,13 +39,13 @@ def generate_time(to_find: str, findtype: List[str]) -> str:
                 break
 
     try:
-        result = (f"<b>Country :</b> <code>{country_name}</code>\n"
-                  f"<b>Zone Name :</b> <code>{country_zone}</code>\n"
-                  f"<b>Country Code :</b> <code>{country_code}</code>\n"
-                  f"<b>Daylight saving :</b> <code>{daylight_saving}</code>\n"
-                  f"<b>Day :</b> <code>{current_day}</code>\n"
-                  f"<b>Current Time :</b> <code>{current_time}</code>\n"
-                  f"<b>Current Date :</b> <code>{current_date}</code>")
+        result = (f"<b>🌏Country :</b> <code>{country_name}</code>\n"
+                  f"<b>⌛️Zone Name :</b> <code>{country_zone}</code>\n"
+                  f"<b>🏖Country Code :</b> <code>{country_code}</code>\n"
+                  f"<b>🌞Daylight saving :</b> <code>{daylight_saving}</code>\n"
+                  f"<b🌤>Day :</b> <code>{current_day}</code>\n"
+                  f"<b>⌚️Current Time :</b> <code>{current_time}</code>\n"
+                  f"<b>📆Current Date :</b> <code>{current_date}</code>")
     except:
         result = None
 
