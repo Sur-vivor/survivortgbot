29773e93ef8fdef126ba946a529dfdc5551acaa2
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-02 15:36:09 +0530

    Update locks.py

--- tg_bot/modules/locks.py
+++ tg_bot/modules/locks.py
@@ -243,9 +243,9 @@ def build_lock_message(chat_id):
     locks = sql.get_locks(chat_id)
     restr = sql.get_restr(chat_id)
     if not (locks or restr):
-        res = tld(chatP.id, "There are no current locks in *{}*.".format(chatname))
+        res = tld(chatP_id, "There are no current locks in *{}*.".format(chatname))
     else:
-        res = tld(chatP.id, "These are the locks in *{}*:".format(chatname))
+        res = tld(chatP_id, "These are the locks in *{}*:".format(chatname))
         if locks:
             res += "\n - sticker = `{}`" \
                    "\n - audio = `{}`" \
