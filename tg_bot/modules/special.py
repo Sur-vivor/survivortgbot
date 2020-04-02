30745bbe6713a9d01115cd3bd8f0f30bed2dd1cf
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-02 11:28:45 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -108,8 +108,8 @@ def leavechat(bot: Bot, update: Update, args: List[int]):
 @run_async
 def slist(bot: Bot, update: Update):
     message = update.effective_message
-    text1 = "<b>My Sudo Users are❤:</b>"
-    text2 = "<b>My Support Users are🧡:</b>"
+    text1 = "<b>My Sudo Users are❤:</b>\n"
+    text2 = "<b>My Support Users are🧡:</b>\n"
     for user_id in SUDO_USERS:
         try:
             user = bot.get_chat(user_id)
