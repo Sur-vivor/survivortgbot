00be503e547951caa35e99ad18ed4a6947d82cbc
Sᴀͥʀᴀͣᴛͫʜ™️ <59915404+Sur-vivor@users.noreply.github.com>
2020-04-11 14:26:22 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -97,21 +97,21 @@ def can_restrict(func):
 
 def owner_plus(func):
     @wraps(func)
-    def is_owner_plus_func(bot: Bot, update: Update, *args, **kwargs):
-
-        user = update.effective_user
-
+    def is_admin(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user  # type: Optional[User]
         if user.id in OWNER_ID:
             return func(bot, update, *args, **kwargs)
+
         elif not user:
             pass
+
         elif DEL_CMDS and " " not in update.effective_message.text:
             update.effective_message.delete()
+
         else:
-            update.effective_message.reply_text("This is a developer restricted command."
-                                                " You do not have permissions to run this.")
-            
-    return is_owner_plus_func        
+            update.effective_message.reply_text("This command is restricted to my owner only.")
+
+    return is_admin     
 
 def sudo_user(func):
     @wraps(func)
